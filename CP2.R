7de2e062d81a2ad16ab388b22a2aedbe73c70d77
Rakus77 <ericrakus@gmail.com>
2026-04-28 22:09:11 -0400

    night commit

--- CP2.R
+++ CP2.R
@@ -16,7 +16,7 @@ install_load <- function(pkgs) {
 }
 
 # Use the function
-install_load(c("tidyverse"))
+install_load(c("tidyverse", "scales"))
 
 
 
@@ -32,7 +32,6 @@ StormData <- StormDataOriginal
 colnames(StormData) <- names_vect
 
 StormData$Date <- as.Date(StormData$Date, format = "%m/%d/%Y")
-#StormData$Event.Type <- as.factor(StormData$Event.Type)
 StormData$Year <- with(StormData, format(Date, "%Y"))
 StormData$Year <- as.numeric(StormData$Year)
 
@@ -63,6 +62,9 @@ StormData$Prop.Dam.Cost <- StormData$Prop.Dam.Significand *
 StormData$Crop.Dam.Cost <- StormData$Crop.Dam.Significand * 
 	StormData$Crop.Dam.Values
 
+StormData$Prop.Dam.Cost <- StormData$Prop.Dam.Cost / (1e12)
+StormData$Crop.Dam.Cost <- StormData$Crop.Dam.Cost / (1e12)
+
 StormData <- subset(StormData, 
 		    select = -c( Prop.Dam.Significand, 
 				 Prop.Dam.Exponent,
@@ -77,7 +79,7 @@ SD <- StormData[!rowSums(StormData[, c(4,5,7,8)]) == 0,]
 
 
 
-A <- unique(SD$Event.Type[grepl("LIGHTNING|THUNDER|TSTM", SD$Event.Type )])
+#A <- unique(SD$Event.Type[grepl("LIGHTNING|THUNDER|TSTM", SD$Event.Type )])
 
 
 
@@ -106,82 +108,167 @@ SD$Event.Type <- gsub(".*OTHER.*|.*\\?.*|.*APACHE.*|.*ACCIDENT.*|.*URBAN.*|.*MIS
 SD$Event.Type <- gsub(".*SEA.*|.*SWELL.*|.*STORM SURGE.*", "MARINE/SEAS", SD$Event.Type)
 SD$Event.Type <- gsub(".*AVALAN.*", "AVALANCHE", SD$Event.Type)
 
+(unique(SD$Event.Type))
 
-
-thunder_lightning_var <- unique(Event.Names[grepl(".*THUN.*|.*LIGHTNING.*|.*TSTM.*|.*LIGNTNING.*|
-			      .*LIGHTING.*|.*MICRO.*|.*DOWNBURST.*", Event.Names )])
-blizzard_var <- unique(Event.Names[grepl(".*BLIZZARD.*", Event.Names )])
-ice_snow_var <- unique(Event.Names[grepl(".*SNOW.*|.*ICE.*|.*WINT.*|.*FROST.*|.*FREEZE.*|.*ICY.*|.*GLAZE.*", Event.Names )])
-tornado_var <- unique(Event.Names[grepl(".*TORNADO.*|.*WATERSPOUT.*|.*DUST.*|.*GUSTNADO.*|.*LANDSPOUT.*", Event.Names )])
-hurricane_var <- unique(Event.Names[grepl(".*HURR.*|.*TYPH.*", Event.Names )])
-hail_var <- unique(Event.Names[grepl(".*HAIL.*", Event.Names )])
-floods_debrisflow_var <- unique(Event.Names[grepl(".*FLOOD.*|.*MUD.*|.*SLIDE.*|.*DAM.*|.*RISING.*", Event.Names )])
-heat_var <- unique(Event.Names[grepl(".*HEAT.*|.*DROUGHT.*|.*HYPER.*|.*WARM.*", Event.Names )])
-rain_sleet_var <- unique(Event.Names[grepl(".*RAIN.*|.*DRIZZ.*|.*SLEET.*|.*PRECIP.*|.*SHOWER.*", Event.Names )])
-wildfire_var <- unique(Event.Names[grepl(".*FIRE.*", Event.Names )])
-fog_var <- unique(Event.Names[grepl(".*FOG.*", Event.Names )])
-cold_wind_var <- unique(Event.Names[grepl(".*COLD.*|.*WIND.*|.*HYPO.*", Event.Names )])
-coastal_surf_tide_var <- unique(Event.Names[grepl(".*SURF.*|.*TIDE.*|.*COAST.*|.*WAVE.*", Event.Names )])
-tropicalstorms_var <- unique(Event.Names[grepl(".*TROPIC.*", Event.Names )])
-other_var <- unique(Event.Names[grepl(".*OTHER.*|.*\\?.*|.*APACHE.*|.*ACCIDENT.*|.*URBAN.*|.*MISHAP.*", Event.Names )])
-marine_seas_var <- unique(Event.Names[grepl(".*SEA.*|.*SWELL.*|.*STORM SURGE.*", Event.Names )])
 
 
 SD$Casualties <- SD$Deaths + SD$Injuries
-SD$Dam.Cost <- (SD$Prop.Dam.Cost + SD$Crop.Dam.Cost) / (1e12)
+SD$Dam.Cost <- (SD$Prop.Dam.Cost + SD$Crop.Dam.Cost)
 
 SD.Agg.Cas <- aggregate(SD$Casualties~SD$Event.Type, FUN = sum)
 names(SD.Agg.Cas) <- c("Event", "Casualties")
 SD.Agg.Dam <- aggregate(SD$Dam.Cost~SD$Event.Type, FUN = sum)
 names(SD.Agg.Dam) <- c("Event", "Damage.Cost")
 
+
+
+
 SD.Agg.DI <- aggregate(cbind(SD$Deaths,SD$Injuries)~SD$Event.Type, FUN = sum)
 names(SD.Agg.DI) <- c("Event", "Deaths", "Injuries")
+SD.Agg.DI$Cas <- SD.Agg.DI$Deaths + SD.Agg.DI$Injuries
+SD.Agg.DI <- slice_max(.data = SD.Agg.DI, order_by = SD.Agg.DI$Cas, n = 10)
+
+
 
 SD.Agg.PC <- aggregate(cbind(SD$Prop.Dam.Cost,SD$Crop.Dam.Cost)~SD$Event.Type, FUN = sum)
 names(SD.Agg.PC) <- c("Event", "Prop.Dam", "Crop.Dam")
+SD.Agg.PC$Dam.Cost <- SD.Agg.PC$Crop.Dam + SD.Agg.PC$Prop.Dam
+SD.Agg.PC <- slice_max(.data = SD.Agg.PC, order_by = SD.Agg.PC$Dam.Cost, n = 10)
 
 
+SD.Agg.byCas <- pivot_longer(data = SD.Agg.DI,cols = c("Deaths", "Injuries"), 
+			     names_to = "Casualty.Type", values_to = "Casualties")
 
-SD.Agg.byCas <- pivot_longer(data = SD.Agg.DI,cols = c("Deaths", "Injuries") , names_to = "Casualty.Type", values_to = "Casualties")
+SD.Agg.byCas$Casualty.Type <- factor(SD.Agg.byCas$Casualty.Type, 
+				     levels = c("Injuries", "Deaths"))
 
-SD.Agg.byCas$Casualty.Type <- factor(SD.Agg.byCas$Casualty.Type, levels = c("Injuries", "Deaths"))
+SD.Agg.byDam <- pivot_longer(data = SD.Agg.PC,cols = c("Prop.Dam", "Crop.Dam"), 
+			     names_to = "Damage.Type", values_to = "Damage.Cost")
 
-
-f <- ggplot(SD.Agg.Cas, aes(x = reorder(Event, -Casualties), y = Casualties, fill = "red"))+
-	geom_bar(stat = "identity")+
-	xlab("Weather Event")+
-	ylab("Casualties")+
-	theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 6), legend.position = "none")+
-	labs(title = "Casualties by Weather Event")
+SD.Agg.byDam$Damage.Type <- factor(SD.Agg.byDam$Damage.Type, 
+				     levels = c("Prop.Dam", "Crop.Dam"))
 
 
-f
-
-h <- ggplot(SD.Agg.Dam, aes(x = reorder(Event, -Damage.Cost), y = Damage.Cost, fill = "red"))+
-	geom_bar(stat = "identity")+
-	xlab("Weather Event")+
-	ylab("Cost of Damage (trillions of dollars)")+
-	theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 6), legend.position = "none")+
-	labs(title = "Economic Impact by Weather Event")
-
-h
 
 
 k <- ggplot(SD.Agg.byCas, aes(x = reorder(Event, -Casualties, sum), y = Casualties, fill = Casualty.Type))+
 	geom_bar(stat = "identity")+
 	xlab("Weather Event")+
 	ylab("Casualties")+
-	theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 6), legend.position = "bottom", legend.title = element_blank())+
-	labs(title = "Casualties by Weather Event")
-
+	theme(plot.title = element_text(hjust = 0.5) , 
+	      axis.text.x = element_text(angle = 45, vjust = 1, 
+	      				hjust = 1, size = 11), legend.position = "bottom", 
+					legend.title = element_blank(), 
+					axis.ticks.length.x = unit(0.5, "cm"))+
+	labs(title = "Casualties by Weather Event")+
+	ylim(0,30000)+
+	scale_x_discrete(labels = c("Tornado", "Thunder\nLightning", 
+			    "Heat", "Floods\nDebris Flow", 
+			    "Ice\nSnow", "Cold\nWind", 
+			    "Wildfire", "Coastal\nSurf/Tide", 
+			    "Hurricane", "Fog"))+
+	stat_summary(fun = sum, aes(label = after_stat(prettyNum(y, big.mark = ",")), group = Event), 
+		     geom = "text", vjust = -0.5)
 
 k
+
+l <- ggplot(SD.Agg.byDam, aes(x = reorder(Event, -Damage.Cost, sum), y = Damage.Cost, fill = Damage.Type))+
+	geom_bar(stat = "identity")+
+	xlab("Weather Event")+
+	ylab("Damages (trillions of dollars)")+
+	theme(plot.title = element_text(hjust = 0.5) , 
+	      axis.text.x = element_text(angle = 45, 
+	      vjust = 1, hjust = 1, size = 11), legend.position = "bottom", 
+	      legend.title = element_blank(),
+	      axis.ticks.length.x = unit(0.5, "cm"))+
+	ylim(0, 50)+
+	labs(title = "Economic Impact by Weather Event")+
+	scale_x_discrete(labels = c("Floods\nDebris Flow", "Tornado", "Hail",
+			"Hurricane",  "Heat", "Thunder\nLightning",
+ 			"Ice\nSnow", "Cold\nWind",
+ 			"Wildfire", "Tropical \n Storms"))+
+	stat_summary(fun = sum, aes(label = dollar(round(after_stat(y), 2)), group = Event), 
+		     geom = "text", vjust = -0.5)
+
+l
+
+
+
+
+
+
+SD.State <- aggregate(cbind(SD$Deaths, SD$Injuries,SD$Casualties,
+			    SD$Prop.Dam.Cost, SD$Crop.Dam.Cost, 
+			    SD$Dam.Cost)~SD$State, FUN = sum)
+names(SD.State) <- c("State", "Deaths", "Injuries", "Property.Damage",
+		     "Crop.Damage", "Total.Damage")
+
+m <- ggplot(SD.State, aes(x = reorder(Event, -Damage.Cost, sum), y = Damage.Cost, fill = Damage.Type))+
+	geom_bar(stat = "identity")+
+	xlab("Weather Event")+
+	ylab("Damages (trillions of dollars)")+
+	theme(plot.title = element_text(hjust = 0.5) , 
+	      axis.text.x = element_text(angle = 45, 
+	      			   vjust = 1, hjust = 1, size = 11), legend.position = "bottom", 
+	      legend.title = element_blank(),
+	      axis.ticks.length.x = unit(0.5, "cm"))+
+	ylim(0, 50)+
+	labs(title = "Economic Impact by Weather Event")+
+	scale_x_discrete(labels = c("Floods\nDebris Flow", "Tornado", "Hail",
+				    "Hurricane",  "Heat", "Thunder\nLightning",
+				    "Ice\nSnow", "Cold\nWind",
+				    "Wildfire", "Tropical \n Storms"))+
+	stat_summary(fun = sum, aes(label = dollar(round(after_stat(y), 2)), group = Event), 
+		     geom = "text", vjust = -0.5)
+
+m
+
+
+
+# 
+# thunder_lightning_var <- unique(Event.Names[grepl(".*THUN.*|.*LIGHTNING.*|.*TSTM.*|.*LIGNTNING.*|
+# 			      .*LIGHTING.*|.*MICRO.*|.*DOWNBURST.*", Event.Names )])
+# blizzard_var <- unique(Event.Names[grepl(".*BLIZZARD.*", Event.Names )])
+# ice_snow_var <- unique(Event.Names[grepl(".*SNOW.*|.*ICE.*|.*WINT.*|.*FROST.*|.*FREEZE.*|.*ICY.*|.*GLAZE.*", Event.Names )])
+# tornado_var <- unique(Event.Names[grepl(".*TORNADO.*|.*WATERSPOUT.*|.*DUST.*|.*GUSTNADO.*|.*LANDSPOUT.*", Event.Names )])
+# hurricane_var <- unique(Event.Names[grepl(".*HURR.*|.*TYPH.*", Event.Names )])
+# hail_var <- unique(Event.Names[grepl(".*HAIL.*", Event.Names )])
+# floods_debrisflow_var <- unique(Event.Names[grepl(".*FLOOD.*|.*MUD.*|.*SLIDE.*|.*DAM.*|.*RISING.*", Event.Names )])
+# heat_var <- unique(Event.Names[grepl(".*HEAT.*|.*DROUGHT.*|.*HYPER.*|.*WARM.*", Event.Names )])
+# rain_sleet_var <- unique(Event.Names[grepl(".*RAIN.*|.*DRIZZ.*|.*SLEET.*|.*PRECIP.*|.*SHOWER.*", Event.Names )])
+# wildfire_var <- unique(Event.Names[grepl(".*FIRE.*", Event.Names )])
+# fog_var <- unique(Event.Names[grepl(".*FOG.*", Event.Names )])
+# cold_wind_var <- unique(Event.Names[grepl(".*COLD.*|.*WIND.*|.*HYPO.*", Event.Names )])
+# coastal_surf_tide_var <- unique(Event.Names[grepl(".*SURF.*|.*TIDE.*|.*COAST.*|.*WAVE.*", Event.Names )])
+# tropicalstorms_var <- unique(Event.Names[grepl(".*TROPIC.*", Event.Names )])
+# other_var <- unique(Event.Names[grepl(".*OTHER.*|.*\\?.*|.*APACHE.*|.*ACCIDENT.*|.*URBAN.*|.*MISHAP.*", Event.Names )])
+# marine_seas_var <- unique(Event.Names[grepl(".*SEA.*|.*SWELL.*|.*STORM SURGE.*", Event.Names )])
+
+
+
+# f <- ggplot(SD.Agg.Cas, aes(x = reorder(Event, -Casualties), y = Casualties, fill = "red"))+
+# 	geom_bar(stat = "identity")+
+# 	xlab("Weather Event")+
+# 	ylab("Casualties")+
+# 	theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 6), legend.position = "none")+
+# 	labs(title = "Casualties by Weather Event")
+# 
+# 
+# f
+# 
+# h <- ggplot(SD.Agg.Dam, aes(x = reorder(Event, -Damage.Cost), y = Damage.Cost, fill = "red"))+
+# 	geom_bar(stat = "identity")+
+# 	xlab("Weather Event")+
+# 	ylab("Cost of Damage (trillions of dollars)")+
+# 	theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 6), legend.position = "none")+
+# 	labs(title = "Economic Impact by Weather Event")
+# 
+# h
 
 
 
 # A <- unique(SD$Event.Type[grepl("LIGHTNING", SD$Event.Type )])
-# length(unique(SD$Event.Type))
+
 
 
 
@@ -380,7 +467,7 @@ k
 # 
 # SD01 <- SD$Event.Type[grepl("Astronomical Low Tide", SD$Event.Type, ignore.case = TRUE)]
 
-
+#StormData$Event.Type <- as.factor(StormData$Event.Type)
 
 
 
